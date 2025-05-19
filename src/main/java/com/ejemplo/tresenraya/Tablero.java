a07754aba80c6dc4c61525f3d2b34900c016a6c9
Joaquin Alfredo Mancilla Aguilar <manjoaquin_03@hotmail.com>
2025-05-19 14:53:20 -0400

    Requerimiento 2

--- src/main/java/com/ejemplo/tresenraya/Tablero.java
+++ src/main/java/com/ejemplo/tresenraya/Tablero.java
@@ -16,6 +16,19 @@ public class Tablero {
 
         tablero[x][y] = "X"; // solo para fines de prueba
     }
+    public void colocarPieza(int x, int y, String jugador) {
+        if (x < 0 || x >= 3) {
+            throw new IllegalArgumentException("Coordenada X fuera del tablero");
+        }
+        if (y < 0 || y >= 3) {
+            throw new IllegalArgumentException("Coordenada Y fuera del tablero");
+        }
+        if (tablero[x][y] != null) {
+            throw new IllegalArgumentException("Ya hay una pieza en esa posición");
+        }
+
+        tablero[x][y] = jugador;
+    }
     public void imprimirTablero() {
     for (int y = 0; y < 3; y++) {
         for (int x = 0; x < 3; x++) {
